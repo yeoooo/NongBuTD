278b3c7d6f26b6c415c3b4d7be0a22ef064f0a53
yeoooo <ruisya330@gmail.com>
2023-04-09 19:38:43 +0900

    add: initial structure

---
+++ server/src/main/java/com/td/NongbuTD/NongbuTdApplication.java
@@ -0,0 +1,13 @@
+package com.td.NongbuTD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongbuTdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NongbuTdApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/td/NongbuTD/NongbuTdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td.NongbuTD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NongbuTdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
