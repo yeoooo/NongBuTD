f2cfbc3b02e285738480640cfc4d138209299698
yeooo <ltbel12@kakao.com>
2023-04-11 01:28:38 +0900

    add: #5 add attribute attack speed

--- server/src/main/java/com/td/NongbuTD/Dto/Tower.java
+++ server/src/main/java/com/td/NongbuTD/Dto/Tower.java
@@ -2,6 +2,7 @@ public class Tower{
     private String name;
     private long attack_pt;
     private long attack_range;
+    private long atk_speed;
     private long price;
     private int level;
 }
