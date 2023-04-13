c7219088adaa8ec85332fd16b74143677c85de1f
yeoooo <ruisya330@gmail.com>
2023-04-13 13:55:18 +0900

    feat: add directories for structure

---
+++ server/src/main/java/com/td/NongbuTD/Domain/Dto/Tower.java
@@ -0,0 +1,4 @@
+package com.td.NongbuTD.Domain.Dto;
+
+public class Tower {
+}
--- server/src/main/java/com/td/NongbuTD/Domain/Dto/TowerDto.java
+++ server/src/main/java/com/td/NongbuTD/Domain/Dto/TowerDto.java
@@ -1,4 +1,6 @@
-public class Tower{
+package com.td.NongbuTD.Domain.Dto;
+
+public class TowerDto {
     private String name;
     private long attack_pt;
     private long attack_range;
