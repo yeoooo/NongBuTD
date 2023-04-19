8445a906f13a4892c7ffae5f0214cf336a4ce6c9
yeoooo <ruisya330@gmail.com>
2023-04-19 11:30:47 +0900

    remove: remove comment already use

--- server/src/main/java/com/td/NongbuTD/domain/User.java
+++ server/src/main/java/com/td/NongbuTD/domain/User.java
@@ -23,11 +23,7 @@ public class User {
     private Long lastClear;
     private Long gold;
     private Long seed;
-    /*
 
-    아이템은, 퀘스트, 농부 캐릭터는 항상 열려있으나 비활성화 상태. 따라서 id만 넣고 활성화해주는
-    편으로 나아가는 것이 적합
-     */
     @OneToMany(mappedBy = "user")
     private List<UserFarmer> farmers;
     @OneToMany(mappedBy = "user")
