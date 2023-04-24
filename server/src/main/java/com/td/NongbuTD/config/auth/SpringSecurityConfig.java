eb732de0b07e8f8320bd7ddcf2c82e9aa91f27e5
yeoooo <ruisya330@gmail.com>
2023-04-24 09:19:02 +0900

    feat: add auth config

--- server/src/main/java/com/td/NongbuTD/config/auth/SpringSecurityConfig.java
+++ server/src/main/java/com/td/NongbuTD/config/auth/SpringSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
-import java.net.http.HttpRequest;
 
 @EnableWebSecurity
 public class SpringSecurityConfig {
